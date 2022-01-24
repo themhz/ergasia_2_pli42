d72c3676fea9d145d82cbb781fd62fc02a069662
themhz <themhz@gmail.com>
2022-01-24 03:23:47 +0200

    first commit

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define N 9
+void main(){
+    int i;
+    char input[N+3];
+    printf("Dwste enan akeraio (mexri %d psifia):",N);
+    fflush(stdout); 
+    fgets(input, sizeof input, stdin);
+    fflush(stdin); 
+    
+}
